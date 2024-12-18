1ca62ecba2ca153d45083517554ea148c51a3600
Denis Galeev <dmdenis74chel@yandex.ru>
2024-12-18 14:36:12 +0500

    Custom User model v1

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Tag(models.Model):
+    pass
+
+
+class Ingredient(models.Model):
+    pass
+
+
+class Recipe(models.Model):
+    pass
--- backend/user/models.py
+++ backend/user/models.py
@@ -10,12 +10,19 @@ from .validators import UsernameValidator, validate_username
 
 class User(AbstractUser):
     """
-    Расширенная стандартная Django модель User -
-    добавлены поля биография и роль пользователя.
+    Расширенная Django модель User -
+    добавлены поля для аватарки и роли пользователя.
     """
+
+    class Role(models.TextChoices):
+        USER = 'user', 'Пользователь'
+        ADMIN = 'admin', 'Администратор'
+
     username = models.CharField(
         max_length=MAX_NAME_FIELD,
         unique=True,
+        blank=False,
+        null=False,
         verbose_name='Имя пользователя',
         help_text=HELP_TEXT_NAME,
         validators=(UsernameValidator(), validate_username,),
@@ -25,29 +32,44 @@ class User(AbstractUser):
     )
     first_name = models.CharField(
         max_length=MAX_NAME_FIELD,
-        blank=True,
-        null=True,
+        blank=False,
+        null=False,
         verbose_name='Имя',
         help_text='Заполните Имя',
-        default='Пусто'
     )
     last_name = models.CharField(
         max_length=MAX_NAME_FIELD,
-        blank=True,
-        null=True,
+        blank=False,
+        null=False,
         verbose_name='Фамилия',
         help_text='Заполните Фамилию',
-        default='Пусто'
     )
     email = models.EmailField(
         max_length=MAX_EMAIL_FIELD,
         unique=True,
+        blank=False,
+        null=False,
         verbose_name='Электронная почта',
         help_text='Введите свой email',
         error_messages={
             'unique': UNIQUE_FIELDS[0],
         },
     )
+    role = models.CharField(
+        max_length=max(len(role) for role, _ in Role.choices),
+        choices=Role.choices,
+        default=Role.USER,
+        verbose_name='Роль пользователя',
+        help_text='Уровень доступа пользователя'
+    )
+    avatar = models.ImageField(
+        upload_to='avatars/',
+        default='avatars/default.png',
+        verbose_name='Аватарка или фото',
+        help_text='Загрузите аватарку или фото',
+    )
+
+    USERNAME_FIELD = 'email'
 
     @property
     def is_admin(self):
@@ -64,3 +86,10 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username[:LENGTH_TEXT]
+
+
+class Subscription(models.Model):
+    """
+    Модель подписок пользователей на других пользователей.
+    """
+    pass
