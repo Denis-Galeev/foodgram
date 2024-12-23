3800908ffcfcc02e9fc321803eaef6dd58a54e70
Denis Galeev <dmdenis74chel@yandex.ru>
2024-12-24 02:19:37 +0500

    Добавил админку юзера

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,14 +1,23 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.admin import ModelAdmin, site
+from django.contrib.admin.decorators import register
+from django.contrib.auth.models import Group
 
 from users.models import User
 
 
-@admin.register(User)
-class UsersAdmin(UserAdmin):
-    """Админка для пользователя"""
+@register(User)
+class UsersAdmin(ModelAdmin):
+    """Админка для модели пользователя"""
 
-    list_display = ('id', 'username', 'email', 'role')
-    search_fields = ('username', 'email')
-    search_help_text = 'Поиск по `username` и `email`'
+    list_display = (
+        'id', 'username', 'email', 'role', 'first_name', 'last_name', 'avatar'
+    )
+    list_editable = ('role', 'avatar')
     list_display_links = ('id', 'username', 'email')
+    list_filter = ('role', 'email')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    search_help_text = 'Поиск по username, email, имени и фамилии'
+
+
+site.empty_value_display = '-- Не задано --'
+site.unregister(Group)
