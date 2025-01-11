8c76198b0124d40d345cb3598dcb45d732c8f500
Denis Galeev <dmdenis74chel@yandex.ru>
2025-01-11 11:28:55 +0500

    1st Review fix_3

--- backend/recipes/management/commands/add_ingr.py
+++ backend/recipes/management/commands/add_ingr.py
@@ -6,13 +6,30 @@ from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
+    help = 'Импорт ингредиентов из CSV файла.'
+
     def handle(self, *args, **options):
         with open('data/ingredients.csv', 'r', encoding='utf-8') as file:
             reader = csv.reader(file, delimiter=',')
-            new_ingredient = [
-                Ingredient(name=name, measurement_unit=unit)
+            existing_ingredients = set(
+                Ingredient.objects.values_list('name', 'measurement_unit')
+            )
+
+            new_ingredients = [
+                Ingredient(name=name.strip(), measurement_unit=unit.strip())
                 for name, unit in reader
-                if name
+                if (name.strip(), unit.strip()) not in existing_ingredients
+                and name
             ]
-            if new_ingredient:
-                Ingredient.objects.bulk_create(new_ingredient)
+
+            if new_ingredients:
+                Ingredient.objects.bulk_create(new_ingredients)
+                self.stdout.write(
+                    self.style.SUCCESS(
+                        f'Добавлено {len(new_ingredients)} новых записей.'
+                    )
+                )
+            else:
+                self.stdout.write(
+                    self.style.SUCCESS('Новых записей для добавления нет.')
+                )
--- backend/recipes/management/commands/add_tags.py
+++ backend/recipes/management/commands/add_tags.py
@@ -6,13 +6,27 @@ from recipes.models import Tag
 
 
 class Command(BaseCommand):
+    help = 'Импорт тегов из CSV файла.'
+
     def handle(self, *args, **options):
         with open('data/tags.csv', 'r', encoding='utf-8') as file:
             reader = csv.reader(file, delimiter=',')
+            existing_slugs = set(Tag.objects.values_list('slug', flat=True))
+
             new_tags = [
                 Tag(name=name, slug=slug)
                 for name, slug in reader
-                if name
+                if slug not in existing_slugs and name
             ]
+
             if new_tags:
                 Tag.objects.bulk_create(new_tags)
+                self.stdout.write(
+                    self.style.SUCCESS(
+                        f'Добавлено {len(new_tags)} новых тегов.'
+                    )
+                )
+            else:
+                self.stdout.write(
+                    self.style.SUCCESS('Новых тегов для добавления нет.')
+                )
